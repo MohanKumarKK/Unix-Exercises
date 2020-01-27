253664006b2c11808ecb403ac4f1bdbf8f979552
MohanKumarKK <60334799+MohanKumarKK@users.noreply.github.com>
2020-01-27 12:38:26 +0530

    Update prog2.sh

--- prog2.sh
+++ prog2.sh
@@ -2,5 +2,4 @@
 echo please ente the file path
 read file_path
 grep 'RuntimeError: Media' $file_path > runtime.txt
-grep -Eo "Media *[0-9]*" < runtime.txt | sort -u
-rm runtime.txt
+grep -Eo "Media *[0-9]*" < runtime.txt | sort -u > media_error.txt
